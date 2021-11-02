37465fb47354e9fc128cbd03b66d1dc6b4854c77
Mark <markglimm@gmail.com>
2021-11-02 16:53:28 -0700

    don't override fallback

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const withImages = require("next-images")
 module.exports = withImages({
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback.fs = false
-      // config.node = {
-      //   fs: "empty",
-      // }
-    }
+    if (!isServer)
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        // config.node = {
+        //   fs: "empty",
+        // }
+      }
     return config
   },
 })
