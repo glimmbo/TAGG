4341093ffe7ac6eabee6fe786a14b5e6426d4bb1
Mark <markglimm@gmail.com>
2021-12-19 12:49:04 -0800

    draft tagg tag

--- components/NavBar.js
+++ components/NavBar.js
@@ -22,11 +22,18 @@ const Nav = styled.nav`
   }
 
   #logo {
-    margin-left: 1em;
-    margin-top: 1em;
+    margin-left: -1em;
+    margin-top: .5em;
+    outline: 1px solid var(--red);
+    padding-left: 1em;
+    box-shadow: 4px 4px var(--red), 0 4px var(--red);
+    background-color: var(--black);
+    max-height: 64px;
+}
+
 
     @media screen and (max-width: 425px) {
-      margin-left: 15px;
+      margin-left: -15px;
     }
   }
 `
@@ -38,12 +45,26 @@ const Links = styled.div`
 `
 
 const Logo = styled.img`
+  margin: 0.5em;
+  margin-left: 1em;
   height: 50px;
   filter: invert(20%) sepia(45%) saturate(6941%) hue-rotate(329deg)
     brightness(95%) contrast(94%);
 
+  :hover {
+    /* to colour var(--grey) */
+    filter: invert(100%) sepia(8%) saturate(316%) hue-rotate(229deg)
+      brightness(79%) contrast(87%);
+  }
+  :active {
+    /* to colour var(--white) */
+    filter: invert(97%) sepia(4%) saturate(700%) hue-rotate(287deg)
+      brightness(118%) contrast(94%);
+  }
+
   @media screen and (max-width: 425px) {
     height: 35px;
+    margin-left: 1em;
   }
 `
 
